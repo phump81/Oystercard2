7b01d59607dc55b6a0cb6e619e7f289f1f520553
Alexis Simonian <alexis792.as@gmail.com>
2020-07-14 16:40:09 +0200

    done exercice 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,11 @@
 class Oystercard
-  attr_reader :balance, :maximum_balance, :in_use
+  attr_reader :balance, :maximum_balance, :entry_station, :exit_station, :trips
   MAXIMUM_BALANCE = 90.00
   MINIMUM_BALANCE = 1.00
   def initialize(balance = 0.00, maximum = MAXIMUM_BALANCE)
     raise('Start balance exceeds your maximum') if
-        (@balance = balance) > (@maximum_balance = maximum)
-    @in_use = false
+      (@balance = balance) > (@maximum_balance = maximum)
+    @trips = []
   end
 
   def top_up(amount)
@@ -15,16 +15,20 @@ class Oystercard
     @balance
   end
  
-  def touch_in
+  def touch_in(first_station)
     raise("Insufficient balance") if @balance < MINIMUM_BALANCE
-    @in_use = true
+    @entry_station = first_station
   end
-  def touch_out(amount=1.00)
-    deduct(amount)
-    @in_use = false
+  def touch_out(exit_station)
+    deduct(MINIMUM_BALANCE)
+    @exit_station = exit_station
+    trip = {entry: @entry_station,
+    exit: @exit_station}
+    @trips << trip
+    @entry_station = nil
   end
   def in_journey?
-    @in_use
+    !!@entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,9 +6,13 @@ describe Oystercard do
   it { expect(subject.balance).to be_an(Float) }
   it { expect(Oystercard::MAXIMUM_BALANCE).to be(max) }
   it { expect(subject.maximum_balance).to be(max) }
+  it { expect(subject.trips).to be_empty }
 end
 
 describe Oystercard do
+  let(:station){ double :station }
+  let(:exit_station){ double :exit_station }
+  let(:trips){ {entry: station, exit: exit_station} }
   it 'adds an amount to the balance' do
     expect(subject.top_up(25)).to eq(subject.balance)
   end
@@ -21,32 +25,51 @@ describe Oystercard do
   end
   it 'deducts ten pounds from the balance' do
     subject.top_up(10)
-    expect{ subject.touch_out(10) }.to change{subject.balance}.by(-10.00)
+    expect{ subject.touch_out(station) }.to change{subject.balance}.by(-1.00)
   end
   it 'raises an error when there is not enough money' do
-    expect{ subject.touch_out(10) }.to raise_error
+    expect{ subject.touch_out(station) }.to raise_error
   end
   it 'set the oystercard in use' do
     card = Oystercard.new(1.00)
-    card.touch_in
-    expect(card.in_use).to eq(true)
+    card.touch_in(station)
+    expect(card.in_journey?).to eq(true)
   end
   it 'set the oystercard not in use' do
     card = Oystercard.new(5.00)
-    card.touch_out
-    expect(card.in_use).to eq(false)
-    expect{ card.touch_out }.to change{card.balance}.by(-1.00)
+    card.touch_out(station)
+    expect(card.in_journey?).to eq(false)
   end
-  
-end
-
-describe Oystercard do
   it 'tells us if in use' do
     card = Oystercard.new(1.99)
-    expect(card.touch_in).to be card.in_journey?
+    card.touch_in(station)
+    expect(card.in_journey?).to eq(true)
   end
   it 'does not allow a balance under one pound' do
     card = Oystercard.new(0.99)
-    expect{ card.touch_in }.to raise_error 'Insufficient balance'
+    expect{ card.touch_in(station) }.to raise_error 'Insufficient balance'
+  end
+  it 'remembers where I touched it' do
+    subject.top_up(50.00)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq(station)
+  end
+  it "forgets entry station when touched out" do
+    subject.top_up(50.00)
+    subject.touch_in(station)
+    subject.touch_out(exit_station)
+    expect(subject.entry_station).to eq(nil)
+  end
+  it "remembers where I touched out" do
+    subject.top_up(50.00)
+    subject.touch_in(station)
+    subject.touch_out(exit_station)
+    expect(subject.exit_station).to eq(exit_station)
+  end
+  it "remembers past trips" do
+    subject.top_up(50.00)
+    subject.touch_in(station)
+    subject.touch_out(exit_station)
+    expect(subject.trips).to include trips
   end
 end
